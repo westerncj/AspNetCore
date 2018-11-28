08dd86640ffbe260878a648d7bf499907a8cfba8
Justin Kotalik <jukotali@microsoft.com>
2018-11-27 16:39:51 -0800

    Get build working

--- src/Servers/IIS/src/AspNetCoreModuleV1/AspNetCore/version.h
+++ src/Servers/IIS/src/AspNetCoreModuleV1/AspNetCore/version.h
@@ -5,9 +5,9 @@
 // This file is auto-generated
 
 
-#define FileVersion 12,1,18331,7
-#define FileVersionStr "12.1.18331.7\0"
-#define ProductVersion 12,1,18331,7
-#define ProductVersionStr "12.1.18331.7\0"
+#define FileVersion 12,1,18332,7
+#define FileVersionStr "12.1.18332.7\0"
+#define ProductVersion 12,1,18332,7
+#define ProductVersionStr "12.1.18332.7\0"
 #define PlatformToolset "v141\0"
 #define CommitHash "\0"
