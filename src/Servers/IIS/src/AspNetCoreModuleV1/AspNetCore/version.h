a20df94618d58162bc33abf4493b471d2c569ef5
Justin Kotalik <jukotali@microsoft.com>
2018-11-28 10:14:47 -0800

    cleanup

--- src/Servers/IIS/src/AspNetCoreModuleV1/AspNetCore/version.h
+++ src/Servers/IIS/src/AspNetCoreModuleV1/AspNetCore/version.h
@@ -10,4 +10,4 @@
 #define ProductVersion 12,1,18332,7
 #define ProductVersionStr "12.1.18332.7\0"
 #define PlatformToolset "v141\0"
-#define CommitHash "\0"
+#define CommitHash "ff50fbb43d39f5a9ac812cd5d1ed26cc62a4f706\0"
